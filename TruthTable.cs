0b9a723ed58c38e89f625bee57759460b0b0ecec
Solveur <bsluch@gmail.com>
2022-09-30 17:41:07 +0300

    Beta release 0.1b

--- TruthTable.cs
+++ TruthTable.cs
@@ -3,13 +3,17 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
+	using System.Linq.Expressions;
+	using System.Reflection;
 	using System.Runtime.CompilerServices;
 	using System.Text.RegularExpressions;
+	using System.Windows.Forms;
+	using static System.Net.Mime.MediaTypeNames;
 
 	public static class TruthTable
 	{
-		// A & !C & (D | !B & (B | !A) | A) & (C | !C)
-		// A * !C * (D + !B * (B + !A) + A) * (C + !C)
+		static readonly Form f = new Form();
+		static readonly DataGridView dgv = new DataGridView();
 
 		//options
 		static readonly char not = '!';
@@ -17,18 +21,17 @@
 		static readonly char closeBracket = ')';
 		static readonly char and = '&';
 		static readonly char or = '|';
-		static readonly char space = ' ';
-		static readonly string[] operators = { $"{and}", $"{or}", $"{not}", $"{openBracket}", $"{closeBracket}", $"{space}" };
-
-		static readonly string expression = "A & !C & (D | !B & (B | !A) | A) & (C | !C)";
-
-
+		static readonly string expression = "(!A & !C) | !A & !C | (D | !B & (B & !A) | A) & (C | !C)"; //A & !C & (D | !B & (B & !A) | A) & (C | !C)
 		static readonly HashSet<string> variables = Variables(expression);
+		static string expressionCode = GetExpressionCode(expression);
+
 
 		static void Main(string[] args)
 		{
 			PrintTable(expression);
-			Console.WriteLine(PrepareString(expression));
+			dgv.Dock = DockStyle.Fill;
+			f.Controls.Add(dgv);
+			f.ShowDialog();
 			Console.Read();
 		}
 
@@ -36,14 +39,23 @@
 		{
 			List<List<string>> matrix = GetMatrix(expression);
 			List<string> head = new List<string>();
-			foreach (var variable in variables) head.Add($"{variable,5}");
+			foreach (var variable in variables)
+			{
+				head.Add($"{variable,3}");
+				dgv.Columns.Add(variable, variable);
+			};
 			matrix = matrix.Prepend(head).ToList();
 
 			matrix.ForEach(row =>
 			{
-				row.ForEach(column => Console.Write($"{column,5}"));
-				Console.WriteLine();
+				dgv.Rows.Add(row.ToArray());
+				row.ForEach(column =>
+				{
+					//Console.Write($"{column,3}");
+				});
+				//Console.WriteLine();
 			});
+			variables.ToList().ForEach(variable => Console.WriteLine(variable));
 		}
 
 		static List<List<string>> GetMatrix(string expression)
@@ -75,56 +87,196 @@
 				}
 			}
 
-			Calculate(matrix, expression);
+			Calculate(matrix, expressionCode);
 			return matrix;
 		}
 
-		static List<List<string>> Calculate(List<List<string>> matrix, string expression)
+		static void Calculate(List<List<string>> matrix, string expressionCodePart)
 		{
-			for (int i = 0; i < expression.Length; i++)
+			Console.WriteLine($"Calculate enter {expressionCodePart}");
+
+			while (expressionCode.Contains($"{openBracket}"))
 			{
-				string expressionCode = PrepareString(expression);
-				//somehow process expressionCode
-				//write all logic functions
+				Console.WriteLine($"Bracket enter   {expressionCodePart} {expressionCode}");
+				string nextBrackets = GetNextBrackets(expressionCodePart);
 
-				char chr = expression[i];
-				//if (!operators.Contains($"{chr}")) variables.Add($"{chr}");
+				string newCodePart = variables.Contains(nextBrackets) ? $"{{{variables.ToList().IndexOf(nextBrackets)}}}" : $"{{{variables.Count()}}}";
 
-				if (variables.Contains($"{chr}")) continue;
-
-				if (chr == not) /*matrix.AddColumn(next => !next)*/;
-				if (chr == and) /*(prev, next) => prev && next*/;
-				if (chr == or) continue;
-				if (chr == openBracket) /*Calculate(matrix, FindBrackets(expression))*/;
-				if (chr == space) continue;
+				CalculateBrackets(matrix, nextBrackets);
+				expressionCodePart = expressionCode;
+				Console.WriteLine($"Bracket exit {newCodePart} {expressionCode}");
 			}
+			// After this line expressionCodePart has no more Brackets
 
-			//	else if (expression[i] == or) /*(prev, next) => prev || next*/;
-			return matrix;
-		}
-
-		public static string FindNextBrackets(string expression)
-		{
-			int startIndex = expression.IndexOf(openBracket);
-			int endIndex = 0;
-			int brackets = 0;
-
-			for (int i = startIndex; i < expression.Length - 1; i++)
+			while (expressionCodePart.Contains($"{not}"))
 			{
-				char c = expression[i];
+				Console.WriteLine($"Not enter {expressionCodePart}");
 
-				if (c == openBracket) brackets++;
-				if (c == closeBracket) brackets--;
-				if (brackets == 0)
+				string[] expressionCodeArray = GetExpressionArray(expressionCodePart);
+				int i = expressionCodeArray.ToList().IndexOf($"{not}");
+
+				string next = expressionCodeArray[i + 1];
+
+				string oldNotCodePart = not + next;
+				string newNotVariable = not + variables.ToList()[GetIndex(next)];
+				string newCodePart = variables.Contains($"{newNotVariable}") ? $"{{{variables.ToList().IndexOf(newNotVariable)}}}" : $"{{{variables.Count()}}}";
+				
+				variables.Add(newNotVariable);
+				for (int x = 0; x < matrix.Count - 1; x++)
 				{
-					endIndex = i + 1;
-					return expression.Substring(startIndex, endIndex - startIndex);
+					matrix[x].Add(ToString(!GetValue(matrix[x], next)));
 				}
+
+				expressionCodePart = expressionCodePart.Replace(oldNotCodePart, newCodePart);
+				Console.WriteLine($"Not exit  {oldNotCodePart} {newCodePart}");
 			}
-			throw new Exception("Wrong brackets arrangement");
+			// After this line expressionCodePart has no more Negation
+
+			while (expressionCodePart.Contains($"{and}"))
+			{
+				Console.WriteLine($"And enter {expressionCodePart}");
+				string[] expressionCodeArray = GetExpressionArray(expressionCodePart);
+				int i = expressionCodeArray.ToList().IndexOf($"{and}");
+				string prev = expressionCodeArray[i - 1];
+				string next = expressionCodeArray[i + 1];
+				string oldAndCodePart = prev + and + next;
+				string newAndVariable = variables.ToList()[GetIndex(prev)] + and + variables.ToList()[GetIndex(next)];
+				string newCodePart = variables.Contains(newAndVariable) ? $"{{{variables.ToList().IndexOf(newAndVariable)}}}" : $"{{{variables.Count()}}}";
+
+				variables.Add(newAndVariable);
+				for (int x = 0; x < matrix.Count - 1; x++)
+				{
+					matrix[x].Add(ToString(GetValue(matrix[x], prev) && GetValue(matrix[x], next)));
+				}
+
+				expressionCodePart = expressionCodePart.Replace(oldAndCodePart, newCodePart);
+				Console.WriteLine($"And exit  {expressionCodePart}");
+			}
+			// After this line expressionCodePart has no more Conjunctions
+
+			while (expressionCodePart.Contains(or))
+			{
+				Console.WriteLine($"Or enter  {expressionCodePart}");
+				string[] expressionCodeArray = GetExpressionArray(expressionCodePart);
+				int i = expressionCodeArray.ToList().IndexOf($"{or}");
+				string prev = expressionCodeArray[i - 1];
+				string next = expressionCodeArray[i + 1];
+				string oldOrCodePart = prev + or + next;
+				string newOrVariable = variables.ToList()[GetIndex(prev)] + or + variables.ToList()[GetIndex(next)];
+				string newCodePart = variables.Contains(newOrVariable) ? $"{{{variables.ToList().IndexOf(newOrVariable)}}}" : $"{{{variables.Count()}}}";
+
+				variables.Add(newOrVariable);
+				for (int x = 0; x < matrix.Count - 1; x++)
+				{
+					matrix[x].Add(ToString(GetValue(matrix[x], prev) || GetValue(matrix[x], next)));
+				}
+
+				expressionCodePart = expressionCodePart.Replace(oldOrCodePart, newCodePart);
+				Console.WriteLine($"Or exit   {expressionCodePart}");
+			}
+			// After this line expressionCodePart has no more Disjunction
+
+			// After this line expressionCodePart has no more operations
+			// (result of brackets calculation)
+			// {4}
+
+			Console.WriteLine($"Calculate exit {expressionCodePart} {expressionCode}");
 		}
 
-		static string PrepareString(string expression)
+		static void CalculateBrackets(List<List<string>> matrix, string bracketsCodePart)
+		{
+			Console.WriteLine($"CalculateBrackets enter {bracketsCodePart}");
+			// After this line expressionCodePart has no more Brackets
+			string oldCodePart = $"({bracketsCodePart})";
+
+			while (bracketsCodePart.Contains($"{not}"))
+			{
+				Console.WriteLine($"Not enter {bracketsCodePart}");
+
+				string[] expressionCodeArray = GetExpressionArray(bracketsCodePart);
+				int i = expressionCodeArray.ToList().IndexOf($"{not}");
+
+				string next = expressionCodeArray[i + 1];
+
+				string oldNotCodePart = not + next;
+				string newNotVariable = not + variables.ToList()[GetIndex(next)];
+				string newCodePart = variables.Contains($"{newNotVariable}") ? $"{{{variables.ToList().IndexOf(newNotVariable)}}}" : $"{{{variables.Count()}}}";
+
+				variables.Add(newNotVariable);
+				for (int x = 0; x < matrix.Count - 1; x++)
+				{
+					matrix[x].Add(ToString(!GetValue(matrix[x], next)));
+				}
+
+				bracketsCodePart = bracketsCodePart.Replace(oldNotCodePart, newCodePart);
+				Console.WriteLine($"Not exit  {oldNotCodePart} {newCodePart}");
+			}
+			// After this line expressionCodePart has no more Negation
+
+			while (bracketsCodePart.Contains($"{and}"))
+			{
+				Console.WriteLine($"And enter {bracketsCodePart}");
+				string[] expressionCodeArray = GetExpressionArray(bracketsCodePart);
+				int i = expressionCodeArray.ToList().IndexOf($"{and}");
+				string prev = expressionCodeArray[i - 1];
+				string next = expressionCodeArray[i + 1];
+				string oldAndCodePart = prev + and + next;
+				string newAndVariable = variables.ToList()[GetIndex(prev)] + and + variables.ToList()[GetIndex(next)];
+				string newCodePart = variables.Contains(newAndVariable) ? $"{{{variables.ToList().IndexOf(newAndVariable)}}}" : $"{{{variables.Count()}}}";
+
+				variables.Add(newAndVariable);
+				for (int x = 0; x < matrix.Count - 1; x++)
+				{
+					matrix[x].Add(ToString(GetValue(matrix[x], prev) && GetValue(matrix[x], next)));
+				}
+
+				bracketsCodePart = bracketsCodePart.Replace(oldAndCodePart, newCodePart);
+				Console.WriteLine($"And exit  {bracketsCodePart}");
+			}
+			// After this line expressionCodePart has no more Conjunctions
+
+			while (bracketsCodePart.Contains(or))
+			{
+				Console.WriteLine($"Or enter  {bracketsCodePart}");
+				string[] expressionCodeArray = GetExpressionArray(bracketsCodePart);
+				int i = expressionCodeArray.ToList().IndexOf($"{or}");
+				string prev = expressionCodeArray[i - 1];
+				string next = expressionCodeArray[i + 1];
+				string oldOrCodePart = prev + or + next;
+				string newOrVariable = variables.ToList()[GetIndex(prev)] + or + variables.ToList()[GetIndex(next)];
+				string newCodePart = variables.Contains(newOrVariable) ? $"{{{variables.ToList().IndexOf(newOrVariable)}}}" : $"{{{variables.Count()}}}";
+
+				variables.Add(newOrVariable);
+				for (int x = 0; x < matrix.Count - 1; x++)
+				{
+					matrix[x].Add(ToString(GetValue(matrix[x], prev) || GetValue(matrix[x], next)));
+				}
+
+				bracketsCodePart = bracketsCodePart.Replace(oldOrCodePart, newCodePart);
+				Console.WriteLine($"Or exit   {bracketsCodePart}");
+			}
+			// After this line expressionCodePart has no more Disjunction
+
+			// After this line expressionCodePart has no more operations
+			// (result of brackets calculation)
+			// {4}
+			expressionCode = expressionCode.Replace(oldCodePart, bracketsCodePart);
+			Console.WriteLine($"CalculateBrackets exit {bracketsCodePart} {expressionCode}");
+		}
+
+		public static string GetNextBrackets(string expression)
+		{
+			if (!expression.Contains(openBracket))
+				return expression;
+
+			if (expression.Count(c => c == openBracket) != expression.Count(c => c == closeBracket))
+				throw new Exception("Wrong bracket arrangement");
+
+			string result = Regex.Match(expression, @"\((\w+|[^()]*)\)").Groups[1].Value;
+			return result;
+		}
+
+		static string GetExpressionCode(string expression)
 		{
 			string expressionCode = expression;
 			expressionCode = expressionCode.Replace(" ", "");
@@ -139,9 +291,36 @@
 			return expressionCode;
 		}
 
+		static string[] GetExpressionArray(string expression)
+		{
+			return Regex.Matches(expression, @"([&|])\1|{\d+}|\S")
+				.Cast<Match>()
+				.Select(match => match.Value)
+				.ToArray();
+		}
+
+
+		static int GetIndex(string expressionPart)
+		{
+			/// Returns index from expressionCodePart like {0}
+			if (expressionPart.Length < 3) throw new Exception($"Wrong expressionPart \"{expressionPart}\" lenght");
+			return Convert.ToInt32(expressionPart.Replace("{", "").Replace("}", ""));
+		}
+
+		static bool GetValue(List<string> row, string expressionPart)
+		{
+			return Convert.ToBoolean(Convert.ToInt32(row[GetIndex(expressionPart)]));
+		}
+
+		static string ToString(bool x)
+		{
+			if (x) return "1";
+			else return "0";
+		}
+
 		static HashSet<string> Variables(string expression)
 		{
-			List<string> temp = Regex.Matches(expression, $@"[^{string.Join(string.Empty, operators)}]+")
+			List<string> temp = Regex.Matches(expression, $@"\w+")
 				.Cast<Match>()
 				.Select(m => m.Value)
 				.OrderBy(x => x)
@@ -149,5 +328,6 @@
 
 			return new HashSet<string>(temp);
 		}
+
 	}
 }
